62abce9b948dda67076a1f0027086356df2087df
Matthew Kennedy <burnsidemk@gmail.com>
2014-09-02 14:50:19 -0500

    Fix description, license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject mintscrape "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject mintscrape "1.0"
+  :description "Automatically export your entire transaction list from https://mint.com"
+  :url "http://github.com/lispnik/mintscrape"
+  :license {:name "Public Domain"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.0.0"]]
   :profiles {:uberjar {:aot :all}}
